5df50980a1e928d9ceb6d36b20be2b9173b6a95b
Dennis Gluesenkamp <data@gluesenkamp.info>
2020-04-02 21:02:05 +0200

    Upload template for homework

---
+++ modules/template_l04.py
@@ -0,0 +1,17 @@
+# choose a specific NFL division (4 teams) from table at https://en.wikipedia.org/wiki/National_Football_League#Teams
+# create dictionary of following structure for chosen division
+division = {
+    'team_id1'  : {
+        'name'  : 'team name',
+        'city'  : 'city of the team',
+        'est'   : 0,
+        'coach' : 'name of head coach'
+    }
+}
+
+def getFoundingYear():
+    # add code for getting founding year of team identified by full team name
+    # should return an integer value (relevant for getOlder() method)
+
+def getOlderTeam():
+    # add code for returning string which explains which team is older
